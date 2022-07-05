2a21cbd384dc3bee4704cdc00b91e5259ca2429c
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-07-05 08:40:26 +0100

    11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,1 +1,18 @@
-#!/usr  
+#!/usr/bin/python3
+
+"""a function that deletes the item
+at a specific position in a list."""
+
+
+def delete_at(my_list=[], idx=0):
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
+    return my_list
+
+    """ SOLUTION TWO
+    try:
+        del my_list[idx]
+    except Exception:
+        error = Exception
+    return my_list
+    """
