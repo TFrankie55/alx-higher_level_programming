409d889b45d1f0d7566ae7014a227193bcd3285e
TFrankie55 <towoeye50@gmail.com>
2022-07-04 17:10:47 -0700

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""a function that retrieves an element in a list like C"""
+
+def element_at(my_list, idx):
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    print("Element at index {:d} is {}".format(idx, my_list))
