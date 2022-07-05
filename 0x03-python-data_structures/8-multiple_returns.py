efb38c956bc321c504b2803e2e659a765de8c867
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-07-05 03:22:35 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,1 +1,11 @@
-#!?usr
+#!/usr/bin/python3
+
+"""a function that returns a tuple with the
+length of a string and its first character."""
+
+
+def multiple_returns(sentence):
+    if sentence == '':
+        return (0, None)
+    else:
+        return(len(sentence), sentence[0])
