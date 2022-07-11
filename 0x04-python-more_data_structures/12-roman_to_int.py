3948fcc544fdba407fe5a48abf45c2cffcb80088
TFrankie55 <towoeye50@gmail.com>
2022-07-11 13:03:22 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 # 12-roman_to_int.py
-# Tosin Owoeye <towoeye50@gmail.com
+# Tosin Owoeye <towoeye50@gmail.com>
 
 """a function that converts a Roman numeral to an integer"""
 
 
 def roman_to_int(roman_string):
-    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000'}
+    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     number = 0
     if (not isinstance(roman_string, str)
             roman_string is None):
         return (0)
     for i in range(len(roman_string)):
-        if conv.get(roman_string[i], 0) == 0:
+        if dic.get(roman_string[i], 0) == 0:
             return (0)
 
         if (i != (len(roman_string) - 1)) and
-                conv[roman_string[i]] < conv[roman_string[i + 1]])
-                num += conv[roman_string[i]] * -1
+                dic[roman_string[i]] < dic[roman_string[i + 1]])
+                num += dic[roman_string[i]] * -1
 
         else:
-            number += conv[roman_string[i]]
+            number += dic[roman_string[i]]
 
     return (number)
