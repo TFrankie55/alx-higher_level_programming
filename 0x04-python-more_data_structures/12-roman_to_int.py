ad88fb4beef6a989ffda06164a39d85385c2986a
TFrankie55 <towoeye50@gmail.com>
2022-07-11 12:51:13 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,22 +6,20 @@
 
 
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str)
-            or roman_string is None):
-        return (0)
-
-    dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000'}
+    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000'}
     number = 0
-
+    if (not isinstance(roman_string, str)
+            roman_string is None):
+        return (0)
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
+        if conv.get(roman_string[i], 0) == 0:
             return (0)
 
         if (i != (len(roman_string) - 1)) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]])
-                num += roman_dict[roman_string[i]] * -1
+                conv[roman_string[i]] < conv[roman_string[i + 1]])
+                num += conv[roman_string[i]] * -1
 
         else:
-            number += roman_dict[roman_string[i]]
+            number += conv[roman_string[i]]
 
     return (number)
