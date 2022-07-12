7081a2378b21065297989c07138e76a8e447b9e6
TFrankie55 <towoeye50@gmail.com>
2022-07-12 02:52:06 -0700

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -5,4 +5,4 @@ value of all integers of a matrix using map"""
 
 
 def square_matrix_map(matrix=[]):
-    return(list(map(lambda x: (list(map(lambda y: y ** 2, [x: ]))) matrix)))
+    return(list(map(lambda x: (list(map(lambda y: y ** 2, x[:]))) matrix)))
