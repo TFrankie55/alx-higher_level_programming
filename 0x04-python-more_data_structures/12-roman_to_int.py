a67d5d5fea1638e57fb8c0cc225157b02c1a9022
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-07-12 10:25:13 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,7 @@ def roman_to_int(roman_string):
             roman_string is None):
         return (0)
 
-    roman_dict = {
+    roman_dic = {
             "I": 1,
             "V": 5,
             "X": 10,
@@ -21,13 +21,13 @@ def roman_to_int(roman_string):
     num = 0
 
     for i in range(len(roman_string)):
-        if roman_dict.get(roman_string[i], 0) == 0:
+        if roman_dic.get(roman_string[i], 0) == 0:
             return (0)
 
         if (i != (len(roman_string) - 1) and
-                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-            num += roman_dict[roman_string[i]] * -1
+                roman_dic[roman_string[i]] < roman_dic[roman_string[i + 1]]):
+            num += roman_dic[roman_string[i]] * -1
 
         else:
-            num += roman_dict[roman_string[i]]
+            num += roman_dic[roman_string[i]]
     return (num)
