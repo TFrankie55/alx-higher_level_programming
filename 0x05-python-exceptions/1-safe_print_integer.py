34574bf5163a8eb9138dc43f313e08a0cf36222d
TFrankie55 <towoeye50@gmail.com>
2022-07-18 20:25:03 -0700

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        if x in my_list is an int and str and float:
+            print("The value of x": x)
+    except IndexError:
+        if my_list is empty:
+            print("The list is empty")
