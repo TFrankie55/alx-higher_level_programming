5642ce54cb61f039db5fc088cd946c779a07b2a0
TFrankie55 <towoeye50@gmail.com>
2022-07-18 20:47:37 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for x in my_list:
-            print("The value of x")
-    except IndexError:
-        if my_list is empty:
-            print("The list is empty")
+    index = 0:
+        while True:
+            try:
+                if index < 1:
+                print(my_list[index], end='')
+                index += 1
+            else:
+                print()
+                return index
+        except IndexError:
+            print()
+            return index
