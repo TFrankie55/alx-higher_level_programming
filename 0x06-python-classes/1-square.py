33c9ac790357673cc33f95fde1679d3d5258e62d
TFrankie55 <towoeye50@gmail.com>
2022-07-19 22:32:11 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,5 +7,6 @@ by: (based on 0-square.py)"""
 class Square:
     """class square that shows private attribute
     size - instantiation with no type/value verification."""
+
     def __init__(self, size):
         self.__self = size
