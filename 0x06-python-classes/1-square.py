0d1d61a41775a47c5cfd5da3bc5107d257f1612d
TFrankie55 <towoeye50@gmail.com>
2022-07-19 22:30:15 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a square
+by: (based on 0-square.py)"""
+
+
+class Square:
+    """class square that shows private attribute
+    size - instantiation with no type/value verification."""
+    def __init__(self, size):
+        self.__self = size
