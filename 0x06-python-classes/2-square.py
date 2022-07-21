c462cffdc211c68172a9956a0190a4eb380a6e6d
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-07-21 08:26:02 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
-"""a class Square that defines a square
-by: (based on 1-square.py)"""
+"""a class Square that defines a
+square by: (based on 1-square.py)"""
 
 
 class Square:
     """Private instance attribute: size
-    Instantiation with optional size: def __init__(self, size=0):"""
+Instantiation with optional size: def __init__(self, size=0):"""
 
     def __init__(self, size=0):
         if type(size) is int:
             if size < 0:
                 raise ValueError("size must be >= 0")
-            self.size = size
+            self.__size = size
         else:
             raise TypeError("size must be an integer")
