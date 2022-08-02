f45b0c78cfca06b4e1d97bc842c268ee350cbf58
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-08-02 02:46:13 +0100

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """Defines an object attribute lookup function."""
 
 
 def lookup(obj):
-    """returns a list of attributes and methods of an obj"""
+    """returns a list of the attributes and methods of obj"""
     return dir(obj)
