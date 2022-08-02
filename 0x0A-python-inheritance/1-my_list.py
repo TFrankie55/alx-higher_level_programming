0ce089d590b24f6a0f494c4301a7fe78e739ded2
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-08-02 02:46:50 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""Defines an inherited list class Mylist"""
+
+"""Defines an inherited list class MyList."""
 
 
-class Mylist(list):
-    """Sorts printing for the built-in list class"""
+class MyList(list):
+    """Implements sorted printing for the built-in list class."""
 
     def print_sorted(self):
-        """prints a list in sorted ascending order"""
+        """Print a list in sorted ascending order."""
         print(sorted(self))
