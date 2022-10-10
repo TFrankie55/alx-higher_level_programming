189779d1f87df13771f3e7d47e934b2eb4436ed3
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-10-10 06:30:39 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,9 +8,9 @@ Usage: ./5-filter_cities.py <mysql username> \
                             <database name> \
                             <state name searched>
 """
-
 import sys
 import MySQLdb
+
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
