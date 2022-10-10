9ec49b5f3959a586e015938fb19840b4a530c904
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-10-10 06:40:01 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""
-    script that takes in the name of a state as an argument and lists
-    all cities of that state, using the database hbtn_0e_4_usa
-"""
-from sys import argv
+# Displays all cities of a given state from the
+# states table of the database hbtn_0e_4_usa.
+# Safe from SQL injections.
+# Usage: ./5-filter_cities.py <mysql username> \
+#                             <mysql password> \
+#                             <database name> \
+#                             <state name searched>
+import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-    cur = conn.cursor()
-    cur.execute("""SELECT * FROM cities INNER JOIN states
-                ON cities.state_id = states.id
-                ORDER BY cities.id""")
-    print(", ".join([city[2] for city in cur.fetchall()
-                     if city[4] == argv[4]]))
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
